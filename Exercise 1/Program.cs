a7f98e352270b475036433e6ea0f063b6501cbe6
peternontembeko <peternontembeko22@gmail.com>
2022-08-02 12:58:15 +0200

    Add project files.

---
+++ Exercise 1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Exercise1
+{
+    internal class Program
+    {
+        static void Main(string[] arg)
+        {
+            int[] myArray = { 1, 2, 3, 4, 5, 6, };
+            bool answer = false;
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                if(myArray[i] % 2 == 0)
+                {
+                    answer = false;
+                }
+                else
+                {
+                    answer = true;
+                }
+                Console.WriteLine("There is an odd number here");
+            }
+            
+        }
+    }
+}
